139c46241b4924e1e205faa08a46229661debda7
saewoonkim <30545132+saewoonkim@users.noreply.github.com>
2023-12-12 17:47:54 +0900

    day4 어레이

---
+++ Test.java
@@ -0,0 +1,19 @@
+package ch05;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str = "ABCDE";
+		String stri = "ABCDE";
+		
+		//char ch = str.charAt(3);
+		//int ch = str.length();
+		//String ch = str.substring(1,4);
+		//boolean ch = str.equals(stri);
+		char[]ch = str.toCharArray();
+		
+		System.out.println(ch);
+
+	}
+
+}
