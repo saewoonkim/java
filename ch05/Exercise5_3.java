863018cedefc6b72f4b2d2d202ddace5eb42eec1
saewoonkim <30545132+saewoonkim@users.noreply.github.com>
2023-12-13 17:43:34 +0900

    day5

---
+++ ch05/Exercise5_3.java
@@ -0,0 +1,16 @@
+package ch05;
+
+public class Exercise5_3 {
+
+	public static void main(String[] args) {
+		int [] arr = {10,20,30,40,50};
+		int sum = 0;
+		
+		for(int i =0; i<arr.length; i++) {
+			sum = sum + arr[i];
+		}
+		
+		System.out.println("sum=" + sum);
+	}
+
+}
